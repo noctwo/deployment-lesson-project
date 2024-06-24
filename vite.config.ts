fcab3f69cc25f155785761b0b2c3bc52c77e01a1
noctwo <mr.evok@gmail.com>
2024-06-24 11:00:15 +0200

    added countries

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve} from "path";
+
 
 export default defineConfig({
-  base: '/deployment-lesson-project/'
-})
+    base: '/deployment-lesson-project/',
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          subpage1: resolve(__dirname, 'src/countries/countries.html')
+        }
+      }
+    }
+});
